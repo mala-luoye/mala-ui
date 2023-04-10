8ad74566328d0c20527eb0e51a7531c1f6f7b05c
lilian <1220795758@qq.com>
2023-04-10 10:40:41 +0800

    feat: integrated build tool Rspack

--- packages/components/button/index.tsx
+++ packages/components/button/index.tsx
@@ -1,11 +1,5 @@
 import React, { FC } from "react"
 
-export interface ButtonProps {
-  chilren?: string
+export const Button: FC = () => {
+  return <div>按钮组件</div>
 }
-
-const Button: FC<ButtonProps> = (props) => {
-  return <div className="mala-button-primary">{props.chilren}</div>
-}
-
-export default Button
---
+++ rspack.config.js
@@ -0,0 +1,33 @@
+const path = require("path")
+const CleanRspackPlugin = require("clean-rspack-plugin")
+
+module.exports = {
+  mode: "production",
+  context: __dirname,
+  entry: {
+    main: "./packages/components/index.ts"
+  },
+  output: {
+    filename: "mala.js",
+    cssFilename: "mala.css",
+    path: path.resolve(__dirname, "./packages/mala-ui/lib")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "less-loader"
+          }
+        ],
+        type: "css"
+      }
+    ]
+  },
+  plugins: [new CleanRspackPlugin()],
+  externals: {
+    react: "react"
+  }
+}

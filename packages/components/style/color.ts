411ec7573f6d2391973b4f4d1f310d54d388a15a
lilian <1220795758@qq.com>
2023-04-16 13:12:41 +0800

    feat: 组件库色彩设计补充

--- packages/components/style/color.ts
+++ packages/components/style/color.ts
@@ -10,29 +10,92 @@
  * primary_color：深竹月
  */
 export const primary_color = "#2578b5"
+export const primary0_color = "#e8f9ff"
+export const primary1_color = "#b9e0f0"
+export const primary2_color = "#8dc7e1"
+export const primary3_color = "#66add3"
+export const primary4_color = "#4493c4"
+export const primary5_color = "#2578b5"
+export const primary6_color = "#18609b"
+export const primary7_color = "#0d4981"
+export const primary8_color = "#053467"
+export const primary9_color = "#00224d"
 
 /**
  * 状态色
  * success_color：鹦鹉绿
- * warning_color：姜黄
- * danger_color：榴花红
- * info_color：承德灰
  */
 export const success_color = "#008e59"
+export const success0_color = "#e8fff3"
+export const success1_color = "#a9e8c9"
+export const success2_color = "#73d2a5"
+export const success3_color = "#44bb88"
+export const success4_color = "#1ea56f"
+export const success5_color = "#008e59"
+export const success6_color = "#007e54"
+export const success7_color = "#006d4c"
+export const success8_color = "#005d44"
+export const success9_color = "#004d3b"
+
+/**
+ * 状态色
+ * warning_color：姜黄
+ */
 export const warning_color = "#b49436"
+export const warning0_color = "#fffde8"
+export const warning1_color = "#f0eabd"
+export const warning2_color = "#e1d696"
+export const warning3_color = "#d2c072"
+export const warning4_color = "#c3ab52"
+export const warning5_color = "#b49436"
+export const warning6_color = "#9a7823"
+export const warning7_color = "#805e13"
+export const warning8_color = "#664508"
+export const warning9_color = "#4d2f00"
+
+/**
+ * 状态色
+ * danger_color：榴花红
+ */
 export const danger_color = "#d54b44"
+export const danger0_color = "#ffede8"
+export const danger1_color = "#f7cdc3"
+export const danger2_color = "#eeaca0"
+export const danger3_color = "#e68c80"
+export const danger4_color = "#dd6b61"
+export const danger5_color = "#d54b44"
+export const danger6_color = "#b32d2b"
+export const danger7_color = "#911719"
+export const danger8_color = "#6f090e"
+export const danger9_color = "#4d0006"
+
+/**
+ * 状态色
+ * info_color：承德灰
+ */
 export const info_color = "#757570"
+export const info0_color = "#fffdf4"
+export const info1_color = "#e3e2da"
+export const info2_color = "#c8c7bf"
+export const info3_color = "#acaca5"
+export const info4_color = "#91908a"
+export const info5_color = "#757570"
+export const info6_color = "#6a6b4d"
+export const info7_color = "#5d612e"
+export const info8_color = "#505715"
+export const info9_color = "#424d00"
 
 /**
  * 中性色
  * gray
  */
-export const gray1_color = "#f7f8fa"
-export const gray2_color = "#f2f3f5"
-export const gray3_color = "#e5e6eb"
-export const gray4_color = "#c9cdd4"
-export const gray5_color = "#a9aeb8"
-export const gray6_color = "#86909c"
-export const gray7_color = "#6b7785"
-export const gray8_color = "#4e5969"
-export const gray9_color = "#272e3b"
+export const gray0_color = "#f7f8fa"
+export const gray1_color = "#f2f3f5"
+export const gray2_color = "#e5e6eb"
+export const gray3_color = "#c9cdd4"
+export const gray4_color = "#a9aeb8"
+export const gray5_color = "#86909c"
+export const gray6_color = "#6b7785"
+export const gray7_color = "#4e5969"
+export const gray8_color = "#272e3b"
+export const gray9_color = "#1d2129"

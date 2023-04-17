8f406ccc0076a74e0882283002f1daa834e8f25a
lilian <1220795758@qq.com>
2023-04-17 19:34:01 +0800

    docs: update docs

---
+++ site/examples/button/type.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Button } from "mala-ui"
+
+const ButtonDemo = () => {
+  return (
+    <div>
+      <Button type="primary">主要按钮</Button>
+      <Button type="success">成功按钮</Button>
+      <Button type="warning">警告按钮</Button>
+      <Button type="danger">危险按钮</Button>
+      <Button type="info">信息按钮</Button>
+    </div>
+  )
+}
+
+export default ButtonDemo
